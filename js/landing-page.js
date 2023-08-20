b3e63d120c1842460e4368b89fd30b814b6fd48f
skwizo <bayimostapha@gmail.com>
2023-08-20 13:12:55 +0100

    fixed v

--- js/landing-page.js
+++ js/landing-page.js
@@ -1,6 +1,6 @@
 var loader = document.querySelector('.loader');
 
-window.addEventListener('load', () => {
+window.addEventListener('DOMContentLoaded', () => {
     loader.style.display = 'none';
     document.body.classList.remove('hide-overflow')
 });
@@ -9,7 +9,7 @@ window.addEventListener('load', () => {
 
 const faders = document.querySelectorAll('.fade-in');
 const appearOptions = {
-    threshold: 0.4
+    threshold: 0.5
 };
 
 const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
@@ -50,15 +50,15 @@ window.addEventListener('scroll', function () {
         radio1.checked = true;
         carousel();
         document.querySelector('.carousel-wrapper').classList.remove('static');
-    } else if (scrollPosition >= viewportHeight / 3 && scrollPosition < viewportHeight / 3 * 2) {
+    } else if (scrollPosition >= viewportHeight / 3 && scrollPosition < (viewportHeight / 3) * 2) {
         radio2.checked = true;
         carousel();
         document.querySelector('.carousel-wrapper').classList.remove('static');
-    } else if (scrollPosition >= viewportHeight / 3 * 2 && scrollPosition < viewportHeight / 3 * 3) {
+    } else if (scrollPosition >= (viewportHeight / 3) * 2 && scrollPosition < viewportHeight) {
         radio3.checked = true;
         carousel();
         document.querySelector('.carousel-wrapper').classList.remove('static');
-    } else if (scrollPosition >= viewportHeight / 3 * 3 && scrollPosition < viewportHeight / 3 * 4) {
+    } else if (scrollPosition >= viewportHeight) {
         document.querySelector('.carousel-wrapper').classList.add('static');
     }
 });
@@ -81,3 +81,15 @@ function carousel() {
         });
     }
 }
+
+// *********************************************
+const ns = document.querySelectorAll('.n');
+let occ = 1;
+
+ns.forEach(n => {
+    if (occ < 10)
+        n.innerHTML = '0' + occ;
+    else
+        n.innerHTML = occ;
+    occ++;
+})
